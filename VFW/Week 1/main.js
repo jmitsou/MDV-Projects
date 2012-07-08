6eb439ad2cc33ad69e1d2c62c6b80244f979d4f7
Jeff Mitsou <jmitsou@fullsail.edu>
2012-07-08 01:38:08 -0400

    added data
    added link events and functions

---
+++ VFW/Week 1/main.js
@@ -0,0 +1,22 @@
+window.addEventListener("DOMContentLoaded", function() {
+
+	//getElementById Function
+	function $(x) {
+		var theElement = document.getElementById(x);
+		return theElement;
+	}
+	
+	
+	//Variable defaults
+	var 
+	
+	
+	//Set Link & Submit Click Events
+	var displayLink = $ ('displayLink');
+	displayLink.addEventListener("click", getData);
+	var clearLink = $ ('clear');
+	clearLink.addEventListener("click", clearLocal);
+	var save = $ ('submit');
+	save.addEventListener("click", storeData);
+	
+});

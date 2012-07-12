319fe332fd800fad6d6d5340304cd069886762d5
Jeff Mitsou <jmitsou@fullsail.edu>
2012-07-12 19:16:55 -0400

    added toggle function

--- VFW/Week 1/main.js
+++ VFW/Week 1/main.js
@@ -44,6 +44,29 @@ window.addEventListener("DOMContentLoaded", function() {
         }
     }
         
+   function toggleControls(n){
+   	switch(n){
+   		case "on":
+   			$("addTask").style.display = "none";
+   			$("clear").style.display = "inline";
+   			$("displayLink").style.display = "none";
+   			$("addNew").style.display = "inline";
+   			break;
+   		case "off":
+   			$("addTask").style.display = "block";
+   			$("clear").style.display = "inline";
+   			$("displayLink").style.display = "inline";
+   			$("addNew").style.display = "none";
+   			$("items").style.display = "none";
+   			
+   			break;
+   		default:
+   			return false;
+   	}
+   
+   }     
+        
+        
     function storeData() {
         console.log("storeData fires");
             var id                          = Math.floor(Math.random()*1000001);

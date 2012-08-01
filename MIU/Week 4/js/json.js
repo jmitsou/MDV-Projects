9579e607a5e2b5ffebaca1cf41cfa9ab4382760a
Jeff Mitsou <jmitsou@fullsail.edu>
2012-07-31 22:57:16 -0400

    CReated week 4

---
+++ MIU/Week 4/js/json.js
@@ -0,0 +1,31 @@
+	var json = {
+	
+		 "marker1":   {
+		 		"group":  ["Group: " , "Book"],
+		 		"date":    ["Date: " , "01-01-2012" ],
+		 		"title":     ["Title: " , "Everybody poops"],
+		 		"chap":   ["Chapter: " , "Stage 1" ],
+		 		"page":   ["Page: " , "1" ],
+		 		"para":    ["Paragraph: " , "3" ],
+		 		"line":     ["Line: " , "50" ],
+		 		"time":    ["Time: " , "01:00" ],
+		 		"notes":  ["Notes: " , "book has fun activities"],
+		 		"read":    ["Type of Reading: " , "Book"],
+		 		"fav":      ["Save as Favorite: " , "Yes"]
+    	 },
+    	 	 
+    	 "marker2":   {
+    		 		"group":  ["Group: " , "Video"],
+    		 		"date":    ["Date: " , "01-01-2012" ],
+    		 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    		 		"chap":   ["Chapter: " , "Stage 1" ],
+    		 		"page":   ["Page: " , "1" ],
+    		 		"para":    ["Paragraph: " , "" ],
+    		 		"line":     ["Line: " , "" ],
+    		 		"time":    ["Time: " , "01:00" ],
+    		 		"notes":  ["Notes: " , "love the videos"],
+    		 		"read":    ["Type of Reading: " , "Book"],
+    		 		"fav":      ["Save as Favorite: " , "Yes"]
+    	  }	 
+    		 	 
+	}

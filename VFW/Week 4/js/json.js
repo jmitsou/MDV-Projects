d767295f09c86e55dc5749427f069456601f4c2f
Jeff Mitsou <jmitsou@fullsail.edu>
2012-07-31 22:35:03 -0400

    Change the name of the file
    week1 to week 1

--- VFW/Week 4/js/json.js
+++ VFW/Week 4/js/json.js
@@ -26,6 +26,258 @@
     		 		"notes":  ["Notes: " , "love the videos"],
     		 		"read":    ["Type of Reading: " , "Book"],
     		 		"fav":      ["Save as Favorite: " , "Yes"]
-    	  }	 
-    		 	 
+    	  },	 
+    	  
+    	  "marker3":   {
+    	  	 		"group":  ["Group: " , "Video"],
+    	  	 		"date":    ["Date: " , "01-01-2012" ],
+    	  	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	  	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	  	 		"page":   ["Page: " , "1" ],
+    	  	 		"para":    ["Paragraph: " , "" ],
+    	  	 		"line":     ["Line: " , "" ],
+    	  	 		"time":    ["Time: " , "01:00" ],
+    	  	 		"notes":  ["Notes: " , "love the videos"],
+    	  	 		"read":    ["Type of Reading: " , "Book"],
+    	  	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	   },	 
+    	  
+    	  "marker4":   {
+    	  	 		"group":  ["Group: " , "Video"],
+    	  	 		"date":    ["Date: " , "01-01-2012" ],
+    	  	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	  	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	  	 		"page":   ["Page: " , "1" ],
+    	  	 		"para":    ["Paragraph: " , "" ],
+    	  	 		"line":     ["Line: " , "" ],
+    	  	 		"time":    ["Time: " , "01:00" ],
+    	  	 		"notes":  ["Notes: " , "love the videos"],
+    	  	 		"read":    ["Type of Reading: " , "Book"],
+    	  	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	   },	 
+    	  
+    	  "marker5":   {
+    	  	 		"group":  ["Group: " , "Video"],
+    	  	 		"date":    ["Date: " , "01-01-2012" ],
+    	  	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	  	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	  	 		"page":   ["Page: " , "1" ],
+    	  	 		"para":    ["Paragraph: " , "" ],
+    	  	 		"line":     ["Line: " , "" ],
+    	  	 		"time":    ["Time: " , "01:00" ],
+    	  	 		"notes":  ["Notes: " , "love the videos"],
+    	  	 		"read":    ["Type of Reading: " , "Book"],
+    	  	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	   },	 
+    	  
+    	  "marker6":   {
+    	  	 		"group":  ["Group: " , "Video"],
+    	  	 		"date":    ["Date: " , "01-01-2012" ],
+    	  	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	  	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	  	 		"page":   ["Page: " , "1" ],
+    	  	 		"para":    ["Paragraph: " , "" ],
+    	  	 		"line":     ["Line: " , "" ],
+    	  	 		"time":    ["Time: " , "01:00" ],
+    	  	 		"notes":  ["Notes: " , "love the videos"],
+    	  	 		"read":    ["Type of Reading: " , "Book"],
+    	  	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	   },	 
+    	  
+    	  "marker7":   {
+    	  	 		"group":  ["Group: " , "Video"],
+    	  	 		"date":    ["Date: " , "01-01-2012" ],
+    	  	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	  	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	  	 		"page":   ["Page: " , "1" ],
+    	  	 		"para":    ["Paragraph: " , "" ],
+    	  	 		"line":     ["Line: " , "" ],
+    	  	 		"time":    ["Time: " , "01:00" ],
+    	  	 		"notes":  ["Notes: " , "love the videos"],
+    	  	 		"read":    ["Type of Reading: " , "Book"],
+    	  	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	   },	 
+    	  
+    	  "marker8":   {
+    	  	 		"group":  ["Group: " , "Video"],
+    	  	 		"date":    ["Date: " , "01-01-2012" ],
+    	  	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	  	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	  	 		"page":   ["Page: " , "1" ],
+    	  	 		"para":    ["Paragraph: " , "" ],
+    	  	 		"line":     ["Line: " , "" ],
+    	  	 		"time":    ["Time: " , "01:00" ],
+    	  	 		"notes":  ["Notes: " , "love the videos"],
+    	  	 		"read":    ["Type of Reading: " , "Book"],
+    	  	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	   },	 
+    	  
+    	  "marker9":   {
+    	  	 		"group":  ["Group: " , "Video"],
+    	  	 		"date":    ["Date: " , "01-01-2012" ],
+    	  	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	  	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	  	 		"page":   ["Page: " , "1" ],
+    	  	 		"para":    ["Paragraph: " , "" ],
+    	  	 		"line":     ["Line: " , "" ],
+    	  	 		"time":    ["Time: " , "01:00" ],
+    	  	 		"notes":  ["Notes: " , "love the videos"],
+    	  	 		"read":    ["Type of Reading: " , "Book"],
+    	  	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	   },	 
+    	  
+    	  "marker10":   {
+    	  	 		"group":  ["Group: " , "Video"],
+    	  	 		"date":    ["Date: " , "01-01-2012" ],
+    	  	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	  	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	  	 		"page":   ["Page: " , "1" ],
+    	  	 		"para":    ["Paragraph: " , "" ],
+    	  	 		"line":     ["Line: " , "" ],
+    	  	 		"time":    ["Time: " , "01:00" ],
+    	  	 		"notes":  ["Notes: " , "love the videos"],
+    	  	 		"read":    ["Type of Reading: " , "Book"],
+    	  	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	   },	 
+    	  
+    	  "marker11":   {
+    	  	 		"group":  ["Group: " , "Video"],
+    	  	 		"date":    ["Date: " , "01-01-2012" ],
+    	  	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	  	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	  	 		"page":   ["Page: " , "1" ],
+    	  	 		"para":    ["Paragraph: " , "" ],
+    	  	 		"line":     ["Line: " , "" ],
+    	  	 		"time":    ["Time: " , "01:00" ],
+    	  	 		"notes":  ["Notes: " , "love the videos"],
+    	  	 		"read":    ["Type of Reading: " , "Book"],
+    	  	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	   },	 
+    	  
+    	  "marker12":   {
+    	  	 		"group":  ["Group: " , "Video"],
+    	  	 		"date":    ["Date: " , "01-01-2012" ],
+    	  	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	  	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	  	 		"page":   ["Page: " , "1" ],
+    	  	 		"para":    ["Paragraph: " , "" ],
+    	  	 		"line":     ["Line: " , "" ],
+    	  	 		"time":    ["Time: " , "01:00" ],
+    	  	 		"notes":  ["Notes: " , "love the videos"],
+    	  	 		"read":    ["Type of Reading: " , "Book"],
+    	  	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	   },	 
+    	  
+    	  "marker13":   {
+    	  	 		"group":  ["Group: " , "Video"],
+    	  	 		"date":    ["Date: " , "01-01-2012" ],
+    	  	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	  	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	  	 		"page":   ["Page: " , "1" ],
+    	  	 		"para":    ["Paragraph: " , "" ],
+    	  	 		"line":     ["Line: " , "" ],
+    	  	 		"time":    ["Time: " , "01:00" ],
+    	  	 		"notes":  ["Notes: " , "love the videos"],
+    	  	 		"read":    ["Type of Reading: " , "Book"],
+    	  	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	   },	 
+    	  
+    	  "marker14":   {
+    	  	 		"group":  ["Group: " , "Video"],
+    	  	 		"date":    ["Date: " , "01-01-2012" ],
+    	  	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	  	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	  	 		"page":   ["Page: " , "1" ],
+    	  	 		"para":    ["Paragraph: " , "" ],
+    	  	 		"line":     ["Line: " , "" ],
+    	  	 		"time":    ["Time: " , "01:00" ],
+    	  	 		"notes":  ["Notes: " , "love the videos"],
+    	  	 		"read":    ["Type of Reading: " , "Book"],
+    	  	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	   },	 
+    	  
+    	  "marker15":   {
+    	  	 		"group":  ["Group: " , "Video"],
+    	  	 		"date":    ["Date: " , "01-01-2012" ],
+    	  	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	  	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	  	 		"page":   ["Page: " , "1" ],
+    	  	 		"para":    ["Paragraph: " , "" ],
+    	  	 		"line":     ["Line: " , "" ],
+    	  	 		"time":    ["Time: " , "01:00" ],
+    	  	 		"notes":  ["Notes: " , "love the videos"],
+    	  	 		"read":    ["Type of Reading: " , "Book"],
+    	  	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	   },
+    	   
+    	   "marker16":   {
+    	   	 		"group":  ["Group: " , "Video"],
+    	   	 		"date":    ["Date: " , "01-01-2012" ],
+    	   	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	   	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	   	 		"page":   ["Page: " , "1" ],
+    	   	 		"para":    ["Paragraph: " , "" ],
+    	   	 		"line":     ["Line: " , "" ],
+    	   	 		"time":    ["Time: " , "01:00" ],
+    	   	 		"notes":  ["Notes: " , "love the videos"],
+    	   	 		"read":    ["Type of Reading: " , "Book"],
+    	   	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	    },	 
+    	   	 
+    	  "marker17":   {
+    	  	 		"group":  ["Group: " , "Video"],
+    	  	 		"date":    ["Date: " , "01-01-2012" ],
+    	  	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	  	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	  	 		"page":   ["Page: " , "1" ],
+    	  	 		"para":    ["Paragraph: " , "" ],
+    	  	 		"line":     ["Line: " , "" ],
+    	  	 		"time":    ["Time: " , "01:00" ],
+    	  	 		"notes":  ["Notes: " , "love the videos"],
+    	  	 		"read":    ["Type of Reading: " , "Book"],
+    	  	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	   },
+    	   
+    	   "marker18":   {
+    	   	 		"group":  ["Group: " , "Video"],
+    	   	 		"date":    ["Date: " , "01-01-2012" ],
+    	   	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	   	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	   	 		"page":   ["Page: " , "1" ],
+    	   	 		"para":    ["Paragraph: " , "" ],
+    	   	 		"line":     ["Line: " , "" ],
+    	   	 		"time":    ["Time: " , "01:00" ],
+    	   	 		"notes":  ["Notes: " , "love the videos"],
+    	   	 		"read":    ["Type of Reading: " , "Book"],
+    	   	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	    },
+    	    
+    	    "marker19":   {
+    	    	 		"group":  ["Group: " , "Video"],
+    	    	 		"date":    ["Date: " , "01-01-2012" ],
+    	    	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	    	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	    	 		"page":   ["Page: " , "1" ],
+    	    	 		"para":    ["Paragraph: " , "" ],
+    	    	 		"line":     ["Line: " , "" ],
+    	    	 		"time":    ["Time: " , "01:00" ],
+    	    	 		"notes":  ["Notes: " , "love the videos"],
+    	    	 		"read":    ["Type of Reading: " , "Book"],
+    	    	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	     },
+    	     
+    	     "marker20":   {
+    	     	 		"group":  ["Group: " , "Video"],
+    	     	 		"date":    ["Date: " , "01-01-2012" ],
+    	     	 		"title":     ["Title: " , "Everybody Poops the Movie"],
+    	     	 		"chap":   ["Chapter: " , "Stage 1" ],
+    	     	 		"page":   ["Page: " , "1" ],
+    	     	 		"para":    ["Paragraph: " , "" ],
+    	     	 		"line":     ["Line: " , "" ],
+    	     	 		"time":    ["Time: " , "01:00" ],
+    	     	 		"notes":  ["Notes: " , "love the videos"],
+    	     	 		"read":    ["Type of Reading: " , "Book"],
+    	     	 		"fav":      ["Save as Favorite: " , "Yes"]
+    	      },
+    	     
 	}

a5d3fe973964d8f6e09ebb71d9c664d73ba08354
Jeff Mitsou <jmitsou@fullsail.edu>
2012-08-14 20:44:42 -0400

    setup week 3 and 4 folders

---
+++ MIU/Week 4/Gold/js/json.js
@@ -0,0 +1,18 @@
+/*  Jeff Mitsou/MIU/Week2/1208  */
+	
+	var json = {
+	
+		"member 1":   {
+				"group":     	["Group: " , "Father"],
+				"fname":    	["First Name: " , "James"],
+				"lname":    	["Last Name: " , "Mitsou"],
+				"bdate":       	["Birthday: " , "02-01-1990" ],
+				"ddate":       	["Date of Death: " , "02-01-1990" ],
+				"age":        	["Age: " , "22"],
+				"sex":   		["Gender: " , "male"],
+				"notes":        ["Notes: " , "2 yrs younger then me"],
+				"fav":            ["Save as Favorite: " , "Yes"]
+		}
+		
+	}
+

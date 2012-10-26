9c0ee575528f2be0c861a9c2560a1cd6c31a9e38
Jeff Mitsou <jmitsou@fullsail.edu>
2012-10-25 23:42:07 -0400

    updated main.js for week 1
    added functions neccesary

--- ASD/Week 1/Gold/js/main.js
+++ ASD/Week 1/Gold/js/main.js
@@ -25,8 +25,89 @@ $('#addmem').on('pageinit', function() {
         });
     };
     
+    function storeData(data) {
+            console.log('storeData');
+    var id = Math.floor(Math.random() * 1000001);
+    // if (!data.key) {
+    // } else {
+    // id = key;
+    // }
+
+            var item = {};
+            item.title = ["Title: ", $('#title').val()];
+            item.fname = ["First Name: ", $('#fname').val()];
+            item.lname = ["Last Name: ", $('#lname').val()];
+            item.bday = ["Birthday: ", $('#bday').val()];
+            item.ddate = ["Death date: ", $('#ddate').val()];
+            item.age = ["Age: ", $('#age').value];
+            item.notes = ["Notes: ", $('#notes').value];
+            item.fav = ["Save as Favorite: ", favValue];
+            item.key = id;
+            //Save data into Local Storage: Use stringify to convert our object to a string.
+            localStorage.setItem(id, JSON.stringify(item));
+            alert("Contact Saved!");
+            changePage("showdata");
+            console.log('changePage');
+            getData();
+            console.log('getData');
+          
+    }
     
-    //clear function
+    function getData() {
+       $('#familyMembers').empty();
+               $.ajax({
+                   url: "js/data.json",
+                   type: "GET",
+                   dataType: "json",
+                   success: function(result) {
+                   		console.log(result);
+                   				$.each(result.rows, function(index, member){
+                   				
+                       				$(''+
+       	                						'<li>'+
+       					                				'<p>'+"Title: "+ member.value.title + '<br />' + '</p>'+
+       					                				'<p>'+"First Name: "+ member.value.fname + '<br />' + '</p>'+
+       					                				'<p>'+"Last Name: "+ member.value.lname + '<br />' + '</p>'+
+       					                				'<p>'+"Birthday: "+ member.value.bday + '<br />' + '</p>'+
+       					                				'<p>'+"Death Date: "+ member.value.ddate + '<br />' + '</p>'+
+       					                				'<p>'+"Age: "+ member.value.age + '<br />' + '</p>'+
+       					                				'<p>'+"Gender: "+ member.value.sex + '<br />' + '</p>'+
+       					                				'<p >'+"Notes: "+ member.value.notes+ '<br />' + '</p>'+
+       					                				'<p>'+"Favorite?: "+ member.value.fav + '<br />' + '</p>'+
+       				                				'</li>'
+                       					
+                       				).appendTo("#familyMembers");
+                       				
+                      				});
+                      				$('#familyMembers').listview('refresh');
+                   },				
+                   	error: function(data) {}
+            	}); 
+     
+    }
+     
+    function autoFillData() {
+    //Store the JSON Object into Local Storage
+    	for(var n in json){
+    		var id 						=Math.floor(Math.random()*1000001);
+    		localStorage.setItem(id, JSON.stringify(json[n]));
+    	}
+    } 
+    
+    //Delete Function
+    function deleteItem() {
+    	 var ask = confirm("Are you sure you want to delete this member?");
+    	 if (ask){
+    	 		localStorage.removeItem(this.key);
+    	 		alert("Member has been deleted.");
+    	 		window.location.reload();
+    	 }else {
+    	 	  alert("Member not deleted.")
+    	 }
+    }
+    
+    
+    //Clear Function
     function clearLocal() {
         if (localStorage.length === 0) {
             alert("There is no data to clear!");
@@ -45,28 +126,9 @@ $('#addmem').on('pageinit', function() {
     
     }
     
-}); // #addmem pageinit ends here
+}); // #addmem pageinit ends here 
 
-//The functions below can go inside or outside the pageinit function for the page in which it is needed.
-
-var autofillData = function (){
-	 
-};
-
-var getData = function(){
-
-};
-
-var storeData = function(data){
-	
-}; 
-
-var	deleteItem = function (){
-			
-};
 					
-var clearLocal = function(){
 
-};
 
 

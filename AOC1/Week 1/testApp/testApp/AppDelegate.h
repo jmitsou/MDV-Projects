fe09881abbfcf8e74a6f5f665cb753e9786d1239
Jeff Mitsou <jmitsou@fullsail.edu>
2013-05-08 23:34:23 -0400

    Initial Commit
    Made folder

---
+++ AOC1/Week 1/testApp/testApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testApp
+//
+//  Created by Jeff Mitsou on 5/8/13.
+//  Copyright (c) 2013 Jeff Mitsou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AOC1/Week 1/testApp/testApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testApp
+//
+//  Created by Jeff Mitsou on 5/8/13.
+//  Copyright (c) 2013 Jeff Mitsou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

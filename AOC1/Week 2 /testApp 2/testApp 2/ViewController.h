f187969647a0c5985973593a79837a52c8803c81
Jeff Mitsou <jmitsou@fullsail.edu>
2013-05-16 16:34:13 -0400

    Title label
    create first label and added title to it.

--- AOC1/Week 2 /testApp 2/testApp 2/ViewController.h
+++ AOC1/Week 2 /testApp 2/testApp 2/ViewController.h
@@ -9,5 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    UILabel * bookTitle;
+
+}
 
 @end
--- AOC1/Week 2 /testApp 2/testApp 2/ViewController.m
+++ AOC1/Week 2 /testApp 2/testApp 2/ViewController.m
@@ -16,6 +16,17 @@
 
 - (void)viewDidLoad
 {
+    //Title Lable for book
+    bookTitle = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 320.0f, 20.0f)];
+    
+    if (bookTitle !=nil) {
+        bookTitle.backgroundColor = [UIColor colorWithRed:0.365 green:0.055 blue:0.035 alpha:1] /*#5d0e09*/;
+        bookTitle.text = @"Catching Fire";
+        bookTitle.textAlignment = NSTextAlignmentCenter;
+        bookTitle.textColor = [UIColor whiteColor];
+    }
+    [self.view addSubview:bookTitle];
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
